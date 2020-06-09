b9e2d33c12b4dd92373806d3b70d69a5f8fa5aef
gene <iamgenechua@gmail.com>
2020-06-09 21:48:37 +0800

    Time 7.45

--- primeTest.c
+++ primeTest.c
@@ -8,7 +8,7 @@ int is_prime(int num) {
         return 1;
     }
 
-    for (int i = 2; i < sqrt(num); i++) {
+    for (int i = 2; i <= sqrt(num); i++) {
         if ((num % i) == 0) {
             return 1;
         }
